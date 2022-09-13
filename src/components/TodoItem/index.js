01da17cae9729a600efa0b9174b1af27650f8f3a
Caezar Olivares <olivarescaezarian@gmail.com>
2022-09-13 23:36:45 +0800

    fix cli errors

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { TodoContext } from '../../Context/Todo';
 
 function TodoItem({item, index}) {
