3cd7b736ef3e5a62e2b3bc3a5285e3fda8017b19
Caezar Olivares <caezar.olivares@crescendocollective.com>
2023-04-18 15:42:49 +0800

    test only

--- src/components/TodoApp/index.js
+++ src/components/TodoApp/index.js
@@ -5,7 +5,7 @@ import { TodoContext } from '../../Context/Todo';
 
 function TodoApp() {
   const [todoItems, setTodoItems] = useState([
-    'Todo List Item #1',
+    'Todo List Item #1 test only',
     'Todo List Item #2',
     'Todo List Item #3',
   ]);
